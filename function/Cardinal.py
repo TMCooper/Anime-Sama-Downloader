9011e689b20c6d5c248130f903e5489f522bf17f
TMCooper <91787869+TMCooper@users.noreply.github.com>
2025-11-05 05:47:31 +0100

    Update Cardinal.py

--- function/Cardinal.py
+++ function/Cardinal.py
@@ -3,17 +3,18 @@ from datetime import datetime
 
 class Cardinal:
    
-    OUTPUT_DIRECTORY = "Anime"
+    OUTPUT_DIRECTORY = "Logs"
     
     @staticmethod # Normalement fonctionelle dans mais peut être voir si dans le cas ou on ne trouve rien pour savoir au moins a quel espisode on est ?
-    def log_error(anime_title, season_number, episode_number, error, languages, langue):
+    def log_error(anime_title, anime_saison, episode_number, error, languages, langue):
         """Enregistre les erreurs dans un fichier de log"""
         os.makedirs(Cardinal.OUTPUT_DIRECTORY, exist_ok=True)
         log_file = os.path.join(Cardinal.OUTPUT_DIRECTORY, "error_log.txt")
         timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        
+        season_num = int(''.join(filter(str.isdigit, anime_saison)))
+
         with open(log_file, 'a', encoding='utf-8') as f:
-            f.write(languages[langue]["cardinalLogs"].format(timestamp=timestamp, anime_title=anime_title, season_number=f"{season_number:02d}", episode_number=f"{episode_number:02d}", error=error))
+            f.write(languages[langue]["cardinalLogs"].format(timestamp=timestamp, anime_title=anime_title, anime_saison=f"{season_num:02d}", episode_number=f"{episode_number:02d}", error=error))
 
     @staticmethod  # <-- Ajouté ici
     def clearScreen():
