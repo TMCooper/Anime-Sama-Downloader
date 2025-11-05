08a82af04d8263f6181085fc64857837305ece76
TMCooper <91787869+TMCooper@users.noreply.github.com>
2025-11-05 05:13:13 +0100

    Update Cardinal.py

--- function/Cardinal.py
+++ function/Cardinal.py
@@ -1,6 +1,6 @@
 import platform, json, os, time
 from datetime import datetime
-from function.Yui import *
+from function.Yui import PATH_LANGUAGE_FOLDER, PATH_LANGUAGE
 
 class Cardinal:
    
@@ -32,7 +32,7 @@ class Cardinal:
     
     @staticmethod
     def creationLanguagesFile(reponse):
-        os.makedirs(Yui.PATH_LANGUAGE_FOLDER, exist_ok=True)
+        os.makedirs(PATH_LANGUAGE_FOLDER, exist_ok=True)
         time.sleep(0.003)
-        with open(Yui.PATH_LANGUAGE, "w", encoding="utf-8") as f:
+        with open(PATH_LANGUAGE, "w", encoding="utf-8") as f:
             json.dump(reponse, f, indent=4, ensure_ascii=False)
