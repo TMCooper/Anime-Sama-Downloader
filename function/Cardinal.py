6aa58ea01f3779538d8bfa72a255470e0806044e
TMCooper <91787869+TMCooper@users.noreply.github.com>
2025-11-05 05:10:29 +0100

    Update Cardinal.py
    + New function

--- function/Cardinal.py
+++ function/Cardinal.py
@@ -1,21 +1,20 @@
-import os
-import platform
+import platform, json, os, time
 from datetime import datetime
-
+from function.Yui import *
 
 class Cardinal:
    
     OUTPUT_DIRECTORY = "Anime"
     
-    @staticmethod
-    def log_error(anime_title, season_number, episode_number, error, langue):
+    @staticmethod # Normalement fonctionelle dans mais peut être voir si dans le cas ou on ne trouve rien pour savoir au moins a quel espisode on est ?
+    def log_error(anime_title, season_number, episode_number, error, languages, langue):
         """Enregistre les erreurs dans un fichier de log"""
         os.makedirs(Cardinal.OUTPUT_DIRECTORY, exist_ok=True)
         log_file = os.path.join(Cardinal.OUTPUT_DIRECTORY, "error_log.txt")
         timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         
         with open(log_file, 'a', encoding='utf-8') as f:
-            f.write(f"[{timestamp}] Échec pour {anime_title} - S{season_number:02d}E{episode_number:02d}: {error}\n")
+            f.write(languages[langue]["cardinalLogs"].format(timestamp=timestamp, anime_title=anime_title, season_number=f"{season_number:02d}", episode_number=f"{episode_number:02d}", error=error))
 
     @staticmethod  # <-- Ajouté ici
     def clearScreen():
@@ -23,3 +22,17 @@ class Cardinal:
             os.system("cls")
         elif platform.system() in ["Linux", "Darwin"]:
             os.system("clear")
+
+    @staticmethod
+    def getLanguages(PATH_LANGUAGE):
+        with open(PATH_LANGUAGE, "r", encoding='utf-8') as data:
+            languages = json.load(data)
+
+        return languages
+    
+    @staticmethod
+    def creationLanguagesFile(reponse):
+        os.makedirs(Yui.PATH_LANGUAGE_FOLDER, exist_ok=True)
+        time.sleep(0.003)
+        with open(Yui.PATH_LANGUAGE, "w", encoding="utf-8") as f:
+            json.dump(reponse, f, indent=4, ensure_ascii=False)
