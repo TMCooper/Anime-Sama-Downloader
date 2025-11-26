16970eeabedc225f03a228496150d9dd029cc008
TMCooper <91787869+TMCooper@users.noreply.github.com>
2025-11-26 15:25:58 +0100

    Update updater.py

--- ressources/updater/updater.py
+++ ressources/updater/updater.py
@@ -7,7 +7,7 @@ def main():
     subprocess.run(["git", "fetch", "origin"]) # Prevoie le cas ou les utilisateur aurais fait des modification au code
     subprocess.run(["git", "reset", "--hard", "origin/main"]) # Rapatrie la dernière version du code
 
-    script_principal = os.path.join(os.path.dirname(__file__), "..", "main.py")  # chemin relatif
+    script_principal = os.path.join(os.path.dirname(__file__), "..", "..", "main.py")  # chemin relatif
     subprocess.Popen([sys.executable, script_principal])
 
 if __name__ == "__main__":
